83134d5bbe6e0eec6ba8bab5a46316e70bd955da
Dan D <degeare28@gmail.com>
2020-01-23 12:32:31 -0700

    converted logic to async/await function

--- master.js
+++ master.js
@@ -1,33 +1,30 @@
+//Packages
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const axios = require("axios");
 
-inquirer
-  .prompt([
-    {
+async function getProfile() {
+  try {
+    const { username } = await inquirer.prompt({
       type: "input",
       message: "What is your GitHub Profile name?",
       name: "username"
-    },
-    {
+    });
+
+    const { color } = await inquirer.prompt({
       type: "list",
       name: "color",
       message: "What is your favorite color?",
       choices: ["Red", "Green", "Blue", "Yellow", "Orange", "Purple"]
-    }
-  ])
-  .then(function({ username }) {
-    let color = response.color;
-    const queryUrl = `https://api.github.com/users/${username}`;
-    console.log(username, color);
-    axios
-      .get(queryUrl)
-      .then(function(res) {
-        console.log(res);
-      })
-      .catch(function(err) {
-        // handle error
-        throw err;
-      });
-  });
+    });
+
+    const { data } = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
+
+    console.log(color, data);
+  } catch (err) {
+    console.log(err);
+  }
+}
